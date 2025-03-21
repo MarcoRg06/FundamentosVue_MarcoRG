7a3527a9f45167e24c0c5b4ecb52ad2d7837ec0f
MarcoRg06 <22620119@tlaxiaco.tecnm.mx>
2025-03-21 15:17:48 -0600

    Agregando carrucel y creando carpetas de imagenes

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 //import HelloWorld from './components/HelloWorld.vue'
 import BarraNavegacion from './components/BarraNavegacion.vue'
 import Textoinformactivo from './components/Textoinformactivo.vue';
+import Carrucel from './components/Carrucel.vue';
 </script>
 
 <template>
 <BarraNavegacion/>
 <Textoinformactivo/>
+<Carrucel/>
 </template>
 
 <style scoped>
